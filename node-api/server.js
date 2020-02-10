708a6ecaffaf32ca807bb831943d7978c7318d40
Wallace <wbescrovaine@gmail.com>
2020-02-10 20:39:56 -0300

    Add Nodemon Dev

--- node-api/server.js
+++ node-api/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Fila da Puta');
+    res.send('Eai Fila da Puta');
 });
 
 app.listen(3001);
