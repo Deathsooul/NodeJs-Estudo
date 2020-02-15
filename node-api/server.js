bbf01428acddf3f62cb5b6e2ae4dca8fd96ffa25
Wallace <wbescrovaine@gmail.com>
2020-02-15 19:55:28 -0300

    Connection with DB

--- node-api/server.js
+++ node-api/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// Inicia o app
 const app = express();
 
+//Inicia DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
+
+// Rota
 app.get('/', (req, res) => {
     res.send('Eai Fila da Puta');
 });
