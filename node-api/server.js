7f10c45b34c605b5457592255d2e060587dd4df6
Wallace <wbescrovaine@gmail.com>
2020-02-16 00:41:26 -0300

    Criando Model de Produto

--- node-api/server.js
+++ node-api/server.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 // Inicia o app
 const app = express();
 
 //Inicia DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
+requireDir("./src/models");
+
+const Product = mongoose.model('Product');
 
 // Rota
 app.get('/', (req, res) => {
-    res.send('Eai Fila da Puta');
+    Product.create({
+        title: 'React Native',
+        description: 'Build Native app with React',
+        url: 'http://github.com/facebook/react-native'
+    });
+    return res.send('Eai Fila da Puta');
 });
 
 app.listen(3001);
---
+++ node-api/src/models/Product.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+mongoose.model('Product', ProductSchema);
