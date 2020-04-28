1fd6b742c15cfedbd36988a6e46adcd7596e4711
Wallace <wbescrovaine@gmail.com>
2020-04-28 10:18:04 -0300

    Delete rite for Incidents

--- bethehero/src/controllers/IncidentController.js
+++ bethehero/src/controllers/IncidentController.js
@@ -1,6 +1,11 @@
 const connection = require('../database/connection');
 
 module.exports = {
+    async index(req, res) {
+        const incidents = await connection('incidents').select('*');
+
+        return res.json(incidents);
+    },
     async create(req, res) {
         const { title, description, value } = req.body;
         const ong_id = req.headers.authorization;
@@ -13,11 +18,20 @@ module.exports = {
         });
         return res.json({ id });
     },
-    async index(req, res) {
-        const incidents = await connection('incidents').select('*');
+    async delete(req, res) {
+        const { id } = req.params;
+        const ong_id = req.headers.authorization;
 
-        return res.json(incidents);
+        const incident = await connection('incidents')
+            .where('id', id)
+            .select('ong_id')
+            .first();
+        if (incident.ong_id !== ong_id) {
+            return res.status(401).json({ error: 'Operation not permited' })
+        }
+        await connection('incidents')
+            .where('id', id).delete();
 
-
+        return res.status(204).send();
     }
 };
--- bethehero/src/routes.js
+++ bethehero/src/routes.js
@@ -37,6 +37,7 @@ routes.post('/ongs', OngController.create);
 
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 
 module.exports = routes;
