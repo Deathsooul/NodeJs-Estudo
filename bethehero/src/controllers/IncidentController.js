b8a57e8e4c87bda87c216991fba00388c2d49d20
Wallace <wbescrovaine@gmail.com>
2020-04-30 09:37:06 -0300

    relacionamento entra as tabelas de casos e ONGS

--- bethehero/src/controllers/IncidentController.js
+++ bethehero/src/controllers/IncidentController.js
@@ -8,9 +8,16 @@ module.exports = {
         console.log(count);
 
         const incidents = await connection('incidents')
+            .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
             .limit(5)
             .offset((page - 1) * 5)
-            .select('*');
+            .select(['incidents.*',
+                'ongs.name',
+                'ongs.email',
+                'ongs.whatsapp',
+                'ongs.city',
+                'ongs.uf'
+            ]);
 
         res.header('X-Total-Count', count['count(*)']);
 
